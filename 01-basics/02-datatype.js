13d98c1fb05d0b6f1bff4d76f930421a283aa55a
Iffat Khan <123642351+IffatK@users.noreply.github.com>
2024-07-13 05:19:55 +0000

    datatypes

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// alert(3+3)   //we are using nodejs not browser
+
+// console.log(3+3); console.log("Iffat khan") // code redability shoould be high
+
+let name ="iffat"
+let age = 20
+let isLoggedIn = true
+
+
+//number => 2 to power of 53
+//bigInt
+//string =>''  ""
+//boolean => true/ false 
+//null => It's a standalone value 
+//undefined =>
+//symbol=> unique
+
+
+//object
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof null)  //object
+// console.log(typeof undefined) //undefined
+
