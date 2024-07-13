37e4db0e782c07980027c83c6058396a923b2f10
Iffat Khan <123642351+IffatK@users.noreply.github.com>
2024-07-13 06:00:48 +0000

    coversions and operations

---
+++ 01-basics/03_conversionsOperations.js
@@ -0,0 +1,42 @@
+let score = 33
+let scoreString  = '33'
+/* 
+console.log(typeof score);
+console.log(typeof scoreString);
+
+let valueInNumber = Number(scoreString)
+console.log(typeof valueInNumber);
+ */
+/* console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2**2)
+console.log(2%2)
+ */
+
+/* let str1 = "Hello"
+let str2 = " Iffat Khan"
+let str3 = str1 + str2
+console.log(str3) */
+
+console.log("1"+1)
+console.log(1+"2")
+
+console.log(typeof "1"+1)
+console.log(typeof 1+"2")
+console.log( 3+4+"1"+2) // number add = 3+ 4 = 7  , string + number = string = 12 , final answer = 712
+
+console.log(+true) // +1
+
+// console.log(true+) // error
+
+// let num1 , num2 , num3 =4
+
+let gameCounter = 100
+gameCounter++ // postfix
+console.log(gameCounter)
+
+
+++gameCounter // profix
+console.log(gameCounter)
