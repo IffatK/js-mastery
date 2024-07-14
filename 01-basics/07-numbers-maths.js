35ee9dc35abe4a514d73aba7d7a2eb5243af0599
Iffat Khan <123642351+IffatK@users.noreply.github.com>
2024-07-14 06:24:44 +0000

    numbers commit

---
+++ 01-basics/07-numbers-maths.js
@@ -0,0 +1,31 @@
+/* const score = 400
+console.log(score)
+const balance = new Number(100)
+console.log(balance)
+
+
+console.log(balance.toString().length)
+
+console.log(typeof balance)
+console.log( balance.toFixed(2))
+
+const otherNumber = 24.97598
+console.log(otherNumber.toPrecision(2)) */
+
+const hundres =100000
+/* console.log(hundres.toLocaleString('en-IN'))
+
+console.log(Math)
+console.log(Math.abs(-3))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.2))
+console.log(Math.min(2,3,4,6,7))
+console.log(Math.max(8,5,7,8,9,2,9)) */
+
+console.log(Math.floor(Math.random()*10) +1 )
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) +min)
+
